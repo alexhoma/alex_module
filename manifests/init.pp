b04e6cf1e91a7517f07f7e54d74a3950b2ce5ce9
Alex Casajuana <alexcm.14@gmail.com>
2015-11-18 21:14:13 +0100

    correccion php

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,28 +23,21 @@ class alex_module {
   }
 
   ## PHP
-  $php_version = '56';
-
   include ::yum::repo::remi
+  package { 'libzip-last':
+    require => Yumrepo['remi']
+  }
 
-  if $php_version == '55' {
-      include ::yum::repo::remi_php55
+  class{ '::yum::repo::remi_php56':
+    require => Package['libzip-last']
   }
-  elsif $php_version == '56'{
-      ::yum::managed_yumrepo { 'remi-php56':
-        descr          => 'Les RPM de remi pour Enterpise Linux $releasever - $basearch - PHP 5.6',
-        mirrorlist     => 'http://rpms.famillecollet.com/enterprise/$releasever/php56/mirror',
-        enabled        => 1,
-        gpgcheck       => 1,
-        gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
-        gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-remi',
-        priority       => 1,
-      }
-  }
+
   class { 'php':
-      version => 'latest',
-      require => Yumrepo['remi-php56']
+    version => 'latest',
+    require => Yumrepo['remi-php56'],
   }
+
+  php::module { [ 'devel', 'pear', 'xml', 'mbstring', 'pecl-memcache', 'soap' ]: }
 
   ## MYSQL
   class { '::mysql::server':

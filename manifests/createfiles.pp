1a51e537af073b0fd8f9befc8a919ebd76a32ad3
Alex Casajuana <alexcm.14@gmail.com>
2015-11-18 20:53:15 +0100

    Cambio forma de llamar al file

--- manifests/createfiles.pp
+++ manifests/createfiles.pp
@@ -1,14 +1,16 @@
 class alex_module::createfiles
 {
-  file { '/var/www/myproject/index.php':
-    ensure  => 'present',
-    source  => "puppet:///modules/alex_module/index.php",
-    mode    => '0644',
+  file {'/var/www/myproject/index.php':
+    ensure => 'present',
+    mode => '0644',
+    content => 'Hello World. Sistema operativo: ${operatingsystem} ${operatingsystemrelease}'
+    owner => 'root',
+    group => 'root',
   }
-
   file { '/var/www/myproject/info.php':
     ensure  => 'present',
     source => "puppet:///modules/alex_module/info.php",
     mode    => '0644',
   }
+
 }
